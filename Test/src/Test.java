83902ab4f19f878bc59d80a4b39095e44f56f91a
Charles Chou <ulc5410@gmail.com>
2021-06-05 14:17:36 -0400

    testing maxPerformance

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,13 +1,21 @@
 import java.util.Arrays;
 
 public class Test {
+
+    public static int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+
+
+        return 0;
+    }
+
     public static void main(String[] args) {
-        String s1 = "ab";
-        String s2 = "dbbca";
-        String s3 = "aadbbcbcac";
+        int n = 6;
+        int [] speed = {2,10,3,1,5,8};
+        int [] efficiency = {5,4,3,9,7,2};
+        int k = 2;
 
         char c = '2';
-        System.out.println( c - '0' );
+        System.out.println( maxPerformance(n, speed, efficiency, k));
     }
 
 }
