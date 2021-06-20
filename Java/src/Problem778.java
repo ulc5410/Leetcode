05b2b7f63293bcc79a7c7e9df9512478ecffb2dd
Charles Chou <ulc5410@gmail.com>
2021-06-20 17:26:42 -0400

    Dijkstra

--- Java/src/Problem778.java
+++ Java/src/Problem778.java
@@ -18,32 +18,31 @@ class Test{
 
     public static int swimInWater(int[][] grid) {
         int [][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        int [][] dp = new int [grid.length][grid.length];
+        int [][] Dijkstra = new int [grid.length][grid.length];
 
-        for (int i = 0; i < dp.length; i++)
-            for(int j = 0; j < dp.length; j++)
-                dp[i][j] = -1;
+        for (int i = 0; i < Dijkstra.length; i++)
+            for(int j = 0; j < Dijkstra.length; j++)
+                Dijkstra[i][j] = -1;
 
         PriorityQueue<Block> q = new PriorityQueue<Block>( new BlockComparator());
-//        Block test = new Block(0,0,0);
         q.add(new Block(0,0,grid[0][0]));
-        dp[0][0] = grid[0][0];
+        Dijkstra[0][0] = grid[0][0];
 
-        while (dp[dp.length-1][dp.length-1] == -1){
+        while (Dijkstra[Dijkstra.length-1][Dijkstra.length-1] == -1){
             Block current = q.poll();
             for (int i = 0; i < directions.length; i++){
                 int a = current.x + directions[i][0];
                 int b = current.y + directions[i][1];
-                if ( (a>=0) && (a<dp.length) && (b>=0) && (b<dp.length) && (dp[a][b] == -1)){
+                if ( (a>=0) && (a<Dijkstra.length) && (b>=0) && (b<Dijkstra.length) && (Dijkstra[a][b] == -1)){
                     Block tmp = new Block(a, b, Math.max(grid[a][b], current.value));
-                    dp[a][b] = tmp.value;
+                    Dijkstra[a][b] = tmp.value;
                     q.add(tmp);
                 }
             }
         }
 
 
-        return dp[dp.length-1][dp.length-1];
+        return Dijkstra[Dijkstra.length-1][Dijkstra.length-1];
     }
 
     public static void main(String[] args) {
