81c17b677a701b59920e5190b4d202e9c82665f7
Charles Chou <ulc5410@gmail.com>
2021-07-07 16:45:00 -0400

    26

--- Java/src/Problem26.java
+++ Java/src/Problem26.java
@@ -1,28 +1,23 @@
-import java.util.Arrays;
-//Remove Duplicates from Sorted Array
-public class Problem26 {
+import java.util.*;
+
+class Test{
     public static int removeDuplicates(int[] nums) {
-//        changes made inplace and return nums.length;
-        if (nums.length == 0) return 0;
-        int j = 0;
-        for (int i = 1; i<nums.length; i = i+1){
-            if (nums[i] != nums[j]){
-                j = j+1;
-                nums[j] = nums[i];
+        if(nums.length == 0) return 0;
+
+        int pos = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[pos-1]){
+                nums[pos] = nums[i];
+                pos += 1;
             }
         }
-        return j+1;
+        return pos;
     }
 
     public static void main(String[] args) {
-//        int [] nums = {0,0,0};
-        int [] nums = {0,0,1,1,1,2,2,3,3,4,7,7};
+        int [] nums = {0,0,1,1,1,2,2,3,3,4};
 
-        int length = removeDuplicates(nums);
-        System.out.println(length);
-        for (int i = 0; i < length; i++) {
-            System.out.println(nums[i]);
-        }
-        System.out.println( Arrays.toString(nums) );
+        System.out.println(removeDuplicates(nums));
     }
 }
+
--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,53 +1,23 @@
 import java.util.*;
 
 class Test{
-    private static int [] merge(int [] a, int [] b){
-        int [] result = new int [a.length+b.length];
-        int pointer_a = 0, pointer_b = 0, pointer_r = 0;
-        while((pointer_a<a.length) && (pointer_b<b.length)){
-            if(a[pointer_a] >= b[pointer_b]){
-                result[pointer_r] = b[pointer_b];
-                pointer_b += 1;
-            }
-            else{
-                result[pointer_r] = a[pointer_a];
-                pointer_a += 1;
-            }
-            pointer_r += 1;
-        }
-        if(pointer_a == a.length){
-            for(int i = pointer_b; i < b.length; i++) {
-                result[pointer_r] = b[pointer_b];
-                pointer_b += 1;
-                pointer_r += 1;
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int pos = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[pos-1]){
+                nums[pos] = nums[i];
+                pos += 1;
             }
         }
-        else{
-            for(int i = pointer_a; i < a.length; i++) {
-                result[pointer_r] = a[pointer_a];
-                pointer_a += 1;
-                pointer_r += 1;
-            }
-        }
-        return result;
-    }
-    public static int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        int [] tmp = matrix[0];
-        for(int i = 1; i < n; i++){
-            tmp = merge(tmp, matrix[i]);
-        }
-        return tmp[k-1];
+        return pos;
     }
 
     public static void main(String[] args) {
-//        int [][] matrix = {{1,5,9},{10,11,13},{12,13,15}};
-//        int k = 8;
-        int [][] matrix = {{11,33,39},{14,15,27},{14,15,17}};
-        int k = 8;
-//        int [][] matrix = {{-5}};
-//        int k = 1;
-        System.out.println(kthSmallest(matrix, k));
+        int [] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(removeDuplicates(nums));
     }
 }
 
