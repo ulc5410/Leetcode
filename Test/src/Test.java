dae79b4e63c081b9c385767ffee2d23c108f0285
Charles Chou <ulc5410@gmail.com>
2021-11-06 23:03:31 -0500

    test

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,45 +1,9 @@
-import java.util.*;
-// Definition for a binary tree node.
-class Solution {
-
-    public boolean solution(int N, int [] A, int [] B) {
-        Map<Integer,List<Integer>> map = new HashMap<>();
-        int M = A.length;
-
-        for(int i = 0; i < M; i++){
-            int source, dest;
-            if(A[i]<B[i]){
-                source = A[i];
-                dest = B[i];
-            }
-            else{
-                source = B[i];
-                dest = A[i];
-            }
-            if (!map.containsKey(source)) {
-                map.put(source, new LinkedList<Integer>());
-            }
-            map.get(source).add(dest);
-        }
-
-        for(int i = 1; i < N; i++){
-            if(!map.get(i).contains(i+1))
-                return false;
-        }
-        return true;
-    }
-}
-
-public class Test {
-    public static void main(String[] args) {
-        Solution obj = new Solution();
-        int N = 4;
-//        int[] A = {1,2,4,4,3};
-//        int[] B = {2,3,1,3,1};
-        int[] A = {1,2,1,3};
-        int[] B = {2,4,3,4};
-        System.out.println(obj.solution(N ,A, B));
-    }
-}
-
-
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+        BEGIN
+        DECLARE M INT;
+        SET M=N-1;
+        RETURN (
+        # Write your MySQL query statement below.
+        SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
+        );
+        END
--- Test/src/Test2.java
+++ Test/src/Test2.java
@@ -1,114 +1,86 @@
-// Java program for kth largest element in a 2d
-// array sorted row-wise and column-wise
-class GFG{
+import java.util.*;
+// Definition for a binary tree node.
+class Solution1 {
 
-    // A structure to store entry of heap.
-// The entry contains value from 2D array,
-// row and column numbers of the value
-    static class HeapNode
-    {
+    public boolean solution(int N, int [] A, int [] B) {
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        int M = A.length;
 
-        // Value to be stored
-        int val;
-
-        // Row number of value in 2D array
-        int r;
-
-        // Column number of value in 2D array
-        int c;
-
-        HeapNode(int val, int r, int c)
-        {
-            this.val = val;
-            this.c = c;
-            this.r = r;
-        }
-    }
-
-    // A utility function to minheapify the node
-// harr[i] of a heap stored in harr[]
-    static void minHeapify(HeapNode harr[],
-                           int i, int heap_size)
-    {
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        int min = i;
-
-        if(l < heap_size&& r<heap_size && harr[l].val < harr[i].val && harr[r].val < harr[i].val){
-            HeapNode temp=harr[r];
-            harr[r]=harr[i];
-            harr[i]=harr[l];
-            harr[l]=temp;
-            minHeapify(harr ,l,heap_size);
-            minHeapify(harr ,r,heap_size);
-        }
-        if (l < heap_size && harr[l].val < harr[i].val){
-            HeapNode temp=harr[i];
-            harr[i]=harr[l];
-            harr[l]=temp;
-            minHeapify(harr ,l,heap_size);
-        }
-    }
-
-    // This function returns kth smallest
-// element in a 2D array mat[][]
-    public static int kthSmallest(int[][] mat,int n, int k)
-    {
-
-        // k must be greater than 0 and
-        // smaller than n*n
-        if (k < 0 && k >= n * n)
-            return Integer.MAX_VALUE;
-
-        // Create a min heap of elements
-        // from first row of 2D array
-        HeapNode harr[] = new HeapNode[n];
-
-        for(int i = 0; i < n; i++)
-        {
-            harr[i] = new HeapNode(mat[0][i], 0, i);
+        for(int i = 0; i < M; i++){
+            int source, dest;
+            if(A[i]<B[i]){
+                source = A[i];
+                dest = B[i];
+            }
+            else{
+                source = B[i];
+                dest = A[i];
+            }
+            if (!map.containsKey(source)) {
+                map.put(source, new LinkedList<Integer>());
+            }
+            map.get(source).add(dest);
         }
 
-        HeapNode hr = new HeapNode(0, 0, 0);
-
-        for(int i = 1; i <= k; i++)
-        {
-
-            // Get current heap root
-            hr = harr[0];
-
-            // Get next value from column of root's
-            // value. If the value stored at root was
-            // last value in its column, then assign
-            // INFINITE as next value
-            int nextVal = hr.r < n - 1 ?
-                    mat[hr.r + 1][hr.c] :
-                    Integer.MAX_VALUE;
-
-            // Update heap root with next value
-            harr[0] = new HeapNode(nextVal,
-                    hr.r + 1, hr.c);
-
-            // Heapify root
-            minHeapify(harr, 0, n);
+        for(int i = 1; i < N; i++){
+            if(!map.containsKey(i)) return false;
+            if(!map.get(i).contains(i+1))
+                return false;
         }
-
-        // Return the value at last extracted root
-        return hr.val;
-    }
-
-    // Driver code
-    public static void main(String args[])
-    {
-        int mat[][] = { { 10, 20, 30, 40 },
-                { 15, 25, 35, 45 },
-                { 25, 29, 37, 48 },
-                { 32, 33, 39, 50 } };
-
-        int res = kthSmallest(mat, 4, 7);
-
-        System.out.print("7th smallest element is "+ res);
+        return true;
     }
 }
 
-// This code is contributed by Rishabh Chauhan
+public class Test2 {
+    public static void main(String[] args) {
+        Solution1 obj = new Solution1();
+        int N = 3;
+//        int[] A = {1,2,4,4,3};
+//        int[] B = {2,3,1,3,1};
+        int[] A = {1,3};
+        int[] B = {2,2};
+        System.out.println(obj.solution(N ,A, B));
+    }
+}
+
+import java.util.*;
+// Definition for a binary tree node.
+class Solution {
+    public int  solution(int A, int B) {
+        if((A+B) < 4) return 0;
+
+//        A is longer
+        if(A < B){
+            int tmp = A;
+            A = B;
+            B = tmp;
+        }
+
+        int tmp, max_e = 0;
+
+        tmp = (int)(A/4);
+        if(tmp > max_e) max_e = tmp;
+
+        tmp = (int)(A/3);
+        if((B >= tmp) && (tmp > max_e)) max_e = tmp;
+        if((B < tmp) && (B > max_e)) max_e = B;
+
+        tmp = (int)(B/2);
+        if(tmp > max_e) max_e = tmp;
+
+        return max_e;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int A = 21;
+        int B = 6;
+        System.out.println(obj.solution(A, B));
+    }
+}
+
+
+
+
